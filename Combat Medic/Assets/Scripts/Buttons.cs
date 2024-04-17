2869244004c464f2ed0c66e3f1a8221bc671b0f4
Kipadd <87720955+Kipadd@users.noreply.github.com>
2024-04-17 21:26:23 +0300

    Add first story
    Add "Lviv attack" story scene with sprites and dialogues

--- Combat Medic/Assets/Scripts/Buttons.cs
+++ Combat Medic/Assets/Scripts/Buttons.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 
@@ -78,6 +79,10 @@ public class Buttons : MonoBehaviour
     public void CloseBtn()
     {
         SettingsPanel.SetActive(false);
+    }
+    public void PlayLevel()
+    {
+        SceneManager.LoadScene(CurrentLevel+1);
     }
 
 }
